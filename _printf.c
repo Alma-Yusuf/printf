dd829b070029f7813653b15a6d86bddd01f593be
Alma-Yusuf <almaabeid2000@gmail.com>
2022-04-20 03:03:51 +0300

    new file

--- _printf.c
+++ _printf.c
@@ -1,47 +1,80 @@
+es (76 sloc)  1.4 KB
+   
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
- */
+  * explore_req - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*explore_req(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	find exp[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"r", print_rev},
+		{"u", print_unsig},
+		{"b", print_bin},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+
+	while (exp[i].var)
+	{
+		if (exp[i].var[0] == (*format))
+			return (exp[i].func);
+		i++;
+	}
+	return (NULL);
+}
+/**
+  * _printf - function that produces output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output text;
+  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list list;
+	int (*f)(va_list);
+	unsigned int i = 0, count = 0;
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	va_start(list, format);
+	while (format[i])
 	{
-		if (*p == '%')
+		while (format[i] != '%' && format[i])
 		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (count);
+		f = explore_req(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(list);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
+	va_end(list);
 	return (count);
 }
